a54a26127a2c94187a87ec5d1926cce42e4bc600
Rodrigo Noschang <rodrigo@hotmail.com>
2022-02-16 18:15:17 -0300

    creating products table

--- src/entities/Cart.ts
+++ src/entities/Cart.ts
@@ -1,10 +1,14 @@
-import { Entity, PrimaryGeneratedColumn, ManyToMany, JoinColumn } from "typeorm";
-// import Product from "./Product";
+import { Entity, PrimaryGeneratedColumn, ManyToMany, JoinTable } from "typeorm";
+import Product from "./Product";
 
 @Entity('carts')
 class Cart {
     @PrimaryGeneratedColumn('uuid')
     id!: string
+
+    @ManyToMany(() => Product)
+    @JoinTable()
+    products!: Product[]
 }
 
 export default Cart;
--- src/entities/Product.ts
+++ src/entities/Product.ts
@@ -1,33 +1,33 @@
-// import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
-// 
-// @Entity('products')
-// class Product {
-//     @PrimaryGeneratedColumn('uuid')
-//     product_id!: string
-// 
-//     @Column()
-//     product_name!: string
-// 
-//     @Column()
-//     product_description!: string
-// 
-//     @Column()
-//     created_at!: Date
-// 
-//     @Column()
-//     updated_at!: Date
-// 
-//     @Column()
-//     price!: number
-// 
-//     @Column()
-//     amount_in_stock!: number
-// 
-//     constructor() {
-//         this.created_at = new Date();
-//         this.updated_at = new Date();
-//         this.product_description = this.product_description || "";
-//     }
-// }
-// 
-// export default Product;
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity('products')
+class Product {
+    @PrimaryGeneratedColumn('uuid')
+    product_id!: string
+
+    @Column()
+    product_name!: string
+
+    @Column()
+    product_description!: string
+
+    @Column()
+    created_at!: Date
+
+    @Column()
+    updated_at!: Date
+
+    @Column()
+    price!: number
+
+    @Column()
+    amount_in_stock!: number
+
+    constructor() {
+        this.created_at = new Date();
+        this.updated_at = new Date();
+        this.product_description = this.product_description || "";
+    }
+}
+
+export default Product;
